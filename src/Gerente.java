8f1d99747f32c2308f716c82f5af4afd345cf296
Nia <estefania.betancur1@soy.sena.edu.co>
2024-05-06 18:49:57 -0500

    Herencia y Polimorfismo - Implementación del Método calcularSalario() en Clase Gerente por Estefania Betancur

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,5 +1,19 @@
 public class Gerente extends Empleado{
+    private double Bono;
+
     public Gerente(String nombre, double salarioBasico) {
         super(nombre, salarioBasico);
+    }
+
+    public double getBono() {
+        return Bono;
+    }
+
+    public void setBono(double bono) {
+        Bono = bono;
+    }
+
+    public double CalcularSalario(){
+        return super.CalcularSalario() + Bono;
     }
 }
--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,5 +1,15 @@
 public class Vendedor extends Empleado{
+    private double Comision;
+
     public Vendedor(String nombre, double salarioBasico) {
         super(nombre, salarioBasico);
+    }
+
+    public double getComision() {
+        return Comision;
+    }
+
+    public void setComision(double comision) {
+        Comision = comision;
     }
 }
